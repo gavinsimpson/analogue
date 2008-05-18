81eef1ea2c9b9f028efb2dbd9c350da2626661e7
Gavin Simpson <ucfagls@gmail.com>
2008-05-18 13:59:41 +0000

    Adds new function tran() to perform common transformations and standardisations

---
+++ pkg/R/tran.R
@@ -0,0 +1,39 @@
+`tran` <- function(x, method, a = 1, b = 0, base = exp(1),
+                   na.rm = FALSE, na.value = 0, ...) {
+    wasDF <- is.data.frame(x)
+    x <- as.matrix(x)
+    METHOD <- c("sqrt", "cubert", "log", "reciprocal", "freq", "center",
+                "standardize", "range", "percent", "proportion", "pa",
+                "missing", "hellinger", "chi.square", "wisconsin",
+                "pcent2prop", "prop2pcent")
+    method <- match.arg(method, METHOD)
+    if(method %in% c("freq", "standardize","range","pa","hellinger",
+                     "chi.square","wisconsin")) {
+        if(isTRUE(all.equal(method, "wisconsin")))
+            x <- wisconsin(x)
+        else
+            x <- decostand(x, method = method, na.rm = na.rm, ...)
+        attr(x, "decostand") <- NULL
+    } else {
+        x <- switch(method,
+                    sqrt = sqrt(x),
+                    cubert = x^(1/3),
+                    log = {x <- sweep(x, 2, a, "*")
+                           x <- sweep(x, 2, b, "+")
+                           log((a * x) + b, base = base)} ,
+                    reciprocal = 1 / x,
+                    center = scale(x, scale = FALSE, center = TRUE),
+                    percent = sweep(x, 1, rowSums(x), "/") * 100,
+                    proportion = sweep(x, 1, rowSums(x), "/"),
+                    missing = apply(x, 2,
+                    function(x) {x[is.na(x)] <- na.value
+                                 x}),
+                    pcent2prop = x / 100,
+                    prop2pcent = x * 100
+                    )
+    }
+    if(wasDF)
+        x <- as.data.frame(x)
+    attr(x, "tran") <- method
+    return(x)
+}
