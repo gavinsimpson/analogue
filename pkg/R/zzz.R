b8455b92eab8d02946895e44a2522ccc361cabcc
Gavin Simpson <ucfagls@gmail.com>
2008-07-19 11:42:12 +0000

    startup message now uses packageStartupMessage()

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,6 +1,6 @@
 .First.lib <- function(lib, pkg)  {
-    cat(paste("This is analogue",
-              utils::packageDescription("analogue")$Version,
-              "\n"))
-    invisible()
+    packageStartupMessage("This is analogue ",
+                          utils::packageDescription("analogue",
+                                                    field="Version"),
+                          appendLF = TRUE)
 }
