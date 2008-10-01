6fd61d8eaf6a90ad3c6dcda33c3f6527a8f7ba37
Gavin Simpson <ucfagls@gmail.com>
2008-10-01 08:14:36 +0000

    Forgot to comment out the library.dynam call in zzz.R

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,5 +1,5 @@
 .First.lib <- function(lib, pkg)  {
-    library.dynam("analogue", pkg, lib)
+    ##library.dynam("analogue", pkg, lib)
     packageStartupMessage("This is analogue ",
                           utils::packageDescription("analogue",
                                                     field="Version"),
