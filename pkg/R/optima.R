be2d81f6341e9b07932c4c2d392f5ae4b538785a
Gavin Simpson <ucfagls@gmail.com>
2009-02-07 12:56:59 +0000

    new print methods for optima and tolerance; additional attributes are now returned.

--- pkg/R/optima.R
+++ pkg/R/optima.R
@@ -7,5 +7,16 @@
     x <- data.matrix(x)
     opt <- colSums(x * env) / colSums(x)
     names(opt) <- colnames(x)
+    class(opt) <- "optima"
+    attr(opt, "env") <- deparse(substitute(env))
     opt
+}
+
+`print.optima` <- function(x, ...) {
+    cat("\n")
+    msg <- paste("Weighted Average Optima For:", attr(x, "env"))
+    writeLines(strwrap(msg, prefix = "\t"),
+               sep = "\n\n")
+    attr(x, "env") <- NULL
+    print(unclass(x), ...)
 }
--- pkg/R/tolerance.R
+++ pkg/R/tolerance.R
@@ -12,5 +12,18 @@
         tol <- tol / sqrt(1 - (1 / N2))
     }
     names(tol) <- colnames(x)
+    class(tol) <- "tolerance"
+    attr(tol, "env") <- deparse(substitute(env))
+    attr(tol, "N2") <- useN2
     tol
+}
+
+`print.tolerance` <- function(x, ...) {
+    cat("\n")
+    msg <- paste("Weighted Average Tolerances For:", attr(x, "env"))
+    writeLines(strwrap(msg, prefix = "\t"),
+               sep = "\n\n")
+    attr(x, "env") <- NULL
+    attr(x, "N2") <- NULL
+    print(unclass(x), ...)
 }
