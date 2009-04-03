a53df13db096124fd5e171af333a81cdf4bf3adf
Gavin Simpson <ucfagls@gmail.com>
2009-04-03 19:33:15 +0000

    as.data.frame methods for optima/tolerance and bump version to 0.6-7

---
+++ pkg/R/as.data.frame.optima.R
@@ -0,0 +1,20 @@
+## simple wrappers to convert optima and tolerance results to
+## data frames
+
+`as.data.frame.optima` <- function(x, row.names = NULL,
+                                   ...) {
+    if(is.null(row.names))
+        row.names <- names(x)
+    res <- data.frame(Opt = as.numeric(x), row.names = row.names,
+                      ...)
+    res
+}
+
+`as.data.frame.tolerance` <- function(x, row.names = NULL,
+                                   ...) {
+    if(is.null(row.names))
+        row.names <- names(x)
+    res <- data.frame(Tol = as.numeric(x), row.names = row.names,
+                      ...)
+    res
+}
