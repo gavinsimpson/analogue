3af689ac8812c5e23e1d7630ce563b5f47ca78bc
Gavin Simpson <ucfagls@gmail.com>
2009-08-16 11:52:47 +0000

    adds left join to 'join' and new argument to supply the value using in replacing missing values.

--- pkg/R/join.R
+++ pkg/R/join.R
@@ -4,8 +4,8 @@
 ##                                                                       ##
 ## Created       : 17-Apr-2006                                           ##
 ## Author        : Gavin Simpson                                         ##
-## Version       : 0.2-0                                                 ##
-## Last modified : 23-Jul-2007                                           ##
+## Version       : 0.5-0                                                 ##
+## Last modified : 16-Aug-2009                                           ##
 ##                                                                       ##
 ## ARGUMENTS:                                                            ##
 ## ...                  - the data frames to be merged                   ##
@@ -35,37 +35,62 @@
 ##                             class data.frame, not that it is that     ##
 ##                             class. This allows join to work with the  ##
 ##                             results of join(..., split = FALSE        ##
+## 16-Aug-2009 - GLS - 0.5-0 * now has left outer as well as outer join  ##
+##                           * replacement value can now be specified    ##
 ##                                                                       ##
 ###########################################################################
-join <- function(..., verbose = FALSE, na.replace = TRUE, split = TRUE)
-  {
+join <- function(..., verbose = FALSE, na.replace = TRUE, split = TRUE,
+                  value = 0, type = c("outer","left"))
+{
+    outerJoin <- function(X) {
+        ## From code provided by Sundar Dorai-Raj in R-Help posting:
+        ## http://article.gmane.org/gmane.comp.lang.r.general/63042/
+        cn <- unique(unlist(lapply(X, colnames)))
+        for(i in seq(along = X)) {
+            if(any(m <- !cn %in% colnames(X[[i]]))) {
+                na <- matrix(NA, nrow(X[[i]]), sum(m))
+                dimnames(na) <- list(rownames(X[[i]]), cn[m])
+                X[[i]] <- cbind(X[[i]], na)
+            }
+        }
+        joined <- do.call(rbind, X)
+        colnames(joined) <- cn
+        return(joined)
+    }
+    leftJoin <- function(X) {
+        cn <- unique(unlist(lapply(X, colnames)[[1]]))
+        ## if more than 2 df in X, merge all bar first
+        if(length(X) > 2)
+            dfs <- outerJoin(X[-1])
+        else
+            dfs <- X[[2]]
+        ## matched column names
+        mcn <- match(colnames(dfs), cn)
+        mcn <- mcn[!is.na(mcn)]
+        joined <- matrix(NA, ncol = dims[1,2], nrow = sum(dims[,1]))
+        joined[1:dims[1,1], ] <- data.matrix(X[[1]])
+        joined[(dims[1,1]+1):NROW(joined), mcn] <- data.matrix(dfs[, mcn])
+        colnames(joined) <- cn
+        return(joined)
+    }
     x <- list(...)
-    ##if(any(sapply(x, class) != "data.frame"))
     if(any(!sapply(x, inherits, "data.frame")))
-      stop("\nall objects to be merged must be data frames.")
+        stop("\nall objects to be merged must be data frames.")
     dims <- do.call(rbind, lapply(x, dim))
     n.joined <- nrow(dims)
-    ## From code provided by Sundar Dorai-Raj in R-Help posting:
-    ## http://article.gmane.org/gmane.comp.lang.r.general/63042/match=merging
-    cn <- unique(unlist(lapply(x, colnames)))
-    for(i in seq(along = x)) {
-      if(any(m <- !cn %in% colnames(x[[i]]))) {
-        na <- matrix(NA, nrow(x[[i]]), sum(m))
-        dimnames(na) <- list(rownames(x[[i]]), cn[m])
-        x[[i]] <- cbind(x[[i]], na)
-      }
+    if(missing(type))
+        type <- "outer"
+    type <- match.arg(type)
+    if(type == "outer") {
+        joined <- outerJoin(x)
+    } else if(type == "left") {
+        joined <- leftJoin(x)
     }
-    joined <- do.call(rbind, x)
-    ## End Sundar code
     if(na.replace) {
-      dim.names <- dimnames(joined)
-      ##joined <- sapply(joined, function(x) {x[is.na(x)] <- 0; x})
-      joined <- data.frame(lapply(joined,
-                                  function(x) {x[is.na(x)] <- 0; x}))
-      dimnames(joined) <- dim.names
+        joined[is.na(joined)] <- value
     }
-    if(verbose)
-      {
+    rn <- lapply(x, rownames)
+    if(verbose) {
         stats <- rbind(dims, dim(joined))
         rownames(stats) <- c(paste("Data set ", c(1:n.joined), ":", sep = ""),
                              "Merged:")
@@ -74,20 +99,21 @@ join <- function(..., verbose = FALSE, na.replace = TRUE, split = TRUE)
         printCoefmat(stats, digits = max(3, getOption("digits") - 3),
                      na.print = "")
         cat("\n")
-      }
-    if(split) {
-      retval <- vector(mode = "list", length = n.joined)
-      ends<- cumsum(dims[,1])
-      start <- c(1, ends[-n.joined] + 1)
-      for(i in 1:n.joined) {
-        retval[[i]] <- as.data.frame(joined[start[i]:ends[i], ])
-        names(retval) <- as.character(match.call())[c(2, 2 + (n.joined-1))]
-      }
-      class(retval) <- "join"
-      return(retval)
-    } else {
-      retval <- as.data.frame(joined, row.names = rownames(joined))
-      class(retval) <- c("join", class(retval))
-      return(retval)
     }
-  }
+    if(split) {
+        retval <- vector(mode = "list", length = n.joined)
+        ends<- cumsum(dims[,1])
+        start <- c(1, ends[-n.joined] + 1)
+        for(i in 1:n.joined) {
+            retval[[i]] <- as.data.frame(joined[start[i]:ends[i], ])
+            rownames(retval[[i]]) <- rn[[i]]
+        }
+        names(retval) <- as.character(match.call())[2:(n.joined+1)]
+        class(retval) <- "join"
+    } else {
+        retval <- as.data.frame(joined, row.names = rownames(joined))
+        class(retval) <- c("join", class(retval))
+    }
+    attr(retval, "type") <- type
+    retval
+}
