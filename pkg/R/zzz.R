b6be84c24dcf87b58156d7f72a6660cec168ab8d
Gavin Simpson <ucfagls@gmail.com>
2011-08-23 08:49:26 +0000

    adds a name space

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,4 +1,11 @@
-.First.lib <- function(lib, pkg)  {
+## .First.lib <- function(lib, pkg)  {
+##     library.dynam("analogue", pkg, lib)
+##     packageStartupMessage("This is analogue ",
+##                           utils::packageDescription("analogue",
+##                                                     field="Version"),
+##                           appendLF = TRUE)
+## }
+.onAttach <- function(lib, pkg)  {
     library.dynam("analogue", pkg, lib)
     packageStartupMessage("This is analogue ",
                           utils::packageDescription("analogue",
