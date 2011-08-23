da2c6efdd96ba58d0dedc6a9466ed375eb0feb60
Gavin Simpson <ucfagls@gmail.com>
2011-08-23 08:43:33 +0000

    extend the print method to handle performance methods that return a matrix of measures (i.e. a set of performance statistics for each axis in PCR)

--- pkg/R/print.performance.R
+++ pkg/R/print.performance.R
@@ -1,8 +1,13 @@
-`print.performance` <- function(x, ...) {
+`print.performance` <- function(x, digits = min(getOption("digits"), 4),
+                                ...) {
     CV.method <- attr(x, "CV.method")
-    perf.names <- names(x)
-    attributes(x) <- NULL
-    names(x) <- perf.names
-    print.default(round(x, 4), ...)
+    if(inherits(x, "data.frame")) {
+        print.data.frame(x, digits = digits, ...)
+    } else {
+        perf.names <- names(x)
+        attributes(x) <- NULL
+        names(x) <- perf.names
+        print.default(x, digits = digits, ...) # x was round(x, 4)
+    }
     invisible(x)
 }
