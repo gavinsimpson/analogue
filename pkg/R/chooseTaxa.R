1938744d9d0307dbf3cbf834d61e3efe7ed82c90
Gavin Simpson <ucfagls@gmail.com>
2013-07-25 16:55:27 +0000

    fix warning, and dropping of empty dimensions

--- pkg/R/chooseTaxa.R
+++ pkg/R/chooseTaxa.R
@@ -9,7 +9,7 @@ chooseTaxa.default <- function(object, n.occ = 1, max.abun = 0,
         type <- "AND"
     type <- match.arg(type)
     ## issue a warning if any values in object are NA
-    if(any(is.na(object)))
+    if(!na.rm && any(is.na(object)))
         warning("'NA's present in data; results may not be what you expect.\nConsider using 'na.rm = TRUE'.")
     occ.want <- colSums(object > 0, na.rm = na.rm) >= n.occ
     abun.want <- apply(object, 2, max, na.rm = na.rm) >= max.abun
@@ -21,7 +21,7 @@ chooseTaxa.default <- function(object, n.occ = 1, max.abun = 0,
     if(value) {
         rname <- rownames(object)
         cname <- colnames(object)
-        object <- object[, want]
+        object <- object[, want, drop = FALSE]
         rownames(object) <- rname
         colnames(object) <- cname[want]
     } else {
