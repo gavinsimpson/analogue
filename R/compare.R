b05c218861ff5d34e5bd599f8d3280cc00f26dde
Gavin Simpson <ucfagls@gmail.com>
2014-06-28 23:42:42 -0600

    adds comparing by samples or species; updated to work with missing species now having N2 = 0; adds new argument n2limit to allow user to define cutoff for species with (potentially) poorly estimated optima

--- R/compare.R
+++ R/compare.R
@@ -19,24 +19,50 @@
 
 ##' @param y data frame; passive or core samples
 ##' @param env numeric vector of environmental or contraint data for residual length ordination
+##' @param by character; compare data sets by sites or species (proxies).
 ##' @param ordination character; which constrained ordination method to use
 ##' @param method character; which dissimilarity method to use. See \code{distance}.
-##' @param transform character: should a transformation be applied to the data. Ignored.
+##' @param transform character; should a transformation be applied to the data. Ignored.
+##' @param n2limit integer; the criterion for indicating species with potentially
+##' poorly estimated optima. The default value of \code{5L} is one suggested by
+##' R. Telford.
 ##'
 ##' @rdname compare
 ##'
 `compare.default` <- function(x, y, env,
+                              by = c("sites", "species"),
                               ordination = "rda",
                               method = "chord",
                               transform = NULL,
+                              n2limit = 5L,
                               ...) {
     joint <- join(x, y, split = TRUE)
     x2 <- joint$x
     y2 <- joint$y
+
     n2.x <- n2(x2, which = "species")
-    out <- data.frame(sumMissing  = rowSums(y2[, is.infinite(n2.x)]),
-                      sumPoorOpt  = rowSums(y2[, n2.x <= 5]),
-                      closestSamp = minDC(ana.fit <- analog(x2, y2))$minDC,
-                      residLen    = residLen(x, env, y, method = ordination)[["passive"]])
+
+    by <- match.arg(by)
+
+    if (identical(by, "species")) {
+        n2.y <- n2(y2, which = "species")
+        cny <- colnames(y)
+        cnx <- colnames(x)
+        out <- data.frame(inTrain  = cny %in% cnx,
+                          n2       = n2.y[cny],
+                          n2Train  = n2.x[cny],
+                          max      = apply(y2, 2, max)[cny],
+                          maxTrain = apply(x2, 2, max)[cny])
+    } else {
+        out <-
+            data.frame(sumMissing  = rowSums(y2[, n2.x == 0L]),
+                       sumPoorOpt  = rowSums(y2[, n2.x <= n2limit]),
+                       closestSamp = minDC(ana.fit <- analog(x2, y2))$minDC)
+        if (!missing(env)) {
+            out <- cbind(out,
+                         residLen = residLen(x, env, y,
+                                             method = ordination)[["passive"]])
+        }
+    }
     out
 }
