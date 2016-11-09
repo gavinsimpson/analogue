e73216f15bc32651a04aeef3187eeb86acbd33ad
Gavin L. Simpson <ucfagls@gmail.com>
2016-11-09 11:35:39 -0600

    add a test for join with type = 'outer'

--- tests/testthat/test-join.R
+++ tests/testthat/test-join.R
@@ -10,11 +10,20 @@ context("Testing join()")
 spp <- data.frame(cat = 1:2, dog = 2:3, fish = 3:4)
 fos <- data.frame(mouse = 7:8, dog = 9:10, cat = 5:6)
 row.names(spp) <- row.names(fos) <- as.character(1:2)
-left <- structure(list(spp = spp,
+LEFT <- structure(list(spp = spp,
                        fos = cbind(fos[, c("cat", "dog")], fish = c(0,0))),
                   class = "join", type = "left")
+OUTER <- structure(list(spp = cbind(spp, mouse = c(0,0)),
+                        fos = cbind(fos[, c("cat", "dog")], fish = c(0,0),
+                        mouse = fos[, "mouse"])),
+                   class = "join", type = "outer")
 
 test_that("join() with type = 'left' works", {
     obj <- join(spp, fos, type = "left")
-    expect_equal(left, obj)
+    expect_equal(LEFT, obj)
+})
+
+test_that("join() with type = 'outer' works", {
+    obj <- join(spp, fos, type = "outer")
+    expect_equal(OUTER, obj)
 })
